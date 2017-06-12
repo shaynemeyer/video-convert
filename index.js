3e84808210bbd5c5c006e6664bd25b8b47c8c543
Shayne Meyer <shayne.meyer@gmail.com>
2017-06-11 17:26:45 -0700

    finished first run of the app

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
+
+const { app, BrowserWindow, ipcMain, shell } = electron;
 
 let mainWindow;
 
@@ -11,4 +14,41 @@ app.on('ready', () => {
   });
 
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+});
+
+ipcMain.on('videos:added', (event, videos) => {
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        video.duration = metadata.format.duration;
+        video.format = 'avi';
+        resolve(video);
+      });
+    });
+  });
+
+  Promise.all(promises)
+      .then((results) => {
+        mainWindow.webContents.send('metadata:complete', results);
+      });
+});
+
+ipcMain.on('conversion:start', (event, videos) => {
+  _.each(videos, video => {
+    const outputDirectory = video.path.split(video.name)[0];
+    const outputName = video.name.split('.')[0];
+    const outputPath = `${outputDirectory}${outputName}.${video.format}`;
+
+    ffmpeg(video.path)
+      .output(outputPath)
+      .on('progress', ({ timemark }) => {
+        mainWindow.webContents.send('conversion:progress', { video, timemark });
+      })
+      .on('end', () => mainWindow.webContents.send('conversion:end', {video, outputPath }))
+      .run();
+  });
+});
+
+ipcMain.on('folder:open', (event, outputPath) => {
+  shell.showItemInFolder(outputPath);
 });
